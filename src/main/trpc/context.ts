55329737262802ef9b67f332fe7bba842815741d
ExilProductions <exil.productions.business@gmail.com>
2026-03-12 16:59:46 +0100

    PKGBUILD only - source pulled from upstream git

--- src/main/trpc/context.ts
+++
@@ -1,7 +0,0 @@
-export type TrpcContext = {
-  senderId?: number
-}
-
-export const createTrpcContext = ({ senderId }: TrpcContext = {}) => ({
-  senderId,
-})
--- src/main/trpc/trpc.ts
+++
@@ -1,4 +0,0 @@
-import { initTRPC } from '@trpc/server'
-import { createTrpcContext } from './context'
-
-export const trpc = initTRPC.context<typeof createTrpcContext>().create()
